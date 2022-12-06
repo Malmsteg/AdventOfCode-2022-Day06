e57140ff431ec8c0dd114ce3c094db39fc95b497
Malmsteg <gustaf.malmsten@gmail.com>
2022-12-06 17:56:45 +0100

    First

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+string file = Path.Combine(currentDirectory, "../../../input.txt");
+string path = Path.GetFullPath(file);
+string[] text = File.ReadAllText(path).Split("\n");
+
+for (int i = 0; i < text[0].Length; i++)
+{
+
+}
